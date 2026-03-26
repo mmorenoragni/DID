f7e22708231e8facb705ef1d36d4c87024ae3932
mmorenoragniiugo <mariano.moreno@iugo.com.uy>
2026-03-26 17:34:01 -0300

    cambio de approach para ejecutar SP

--- ejecucion_sp_nuevopaquetes.sql
+++ ejecucion_sp_nuevopaquetes.sql
@@ -1,101 +1,88 @@
-SET SERVEROUTPUT ON;
-
 DECLARE
-  l_rc SYS_REFCURSOR;
+  v_cursor SYS_REFCURSOR;
 
-  -- 31 columnas (mismo orden que tu SELECT del SP)
-  v_cui_dni            VARCHAR2(50);
-  v_prenombres         VARCHAR2(200);
-  v_apellido_paterno  VARCHAR2(200);
-  v_apellido_materno  VARCHAR2(200);
-  v_apellido_casada   VARCHAR2(200);
-  v_fecha_nacimiento  DATE;
+  TYPE t_dni IS RECORD (
+    cui_dni                 VARCHAR2(50),
+    prenombres              VARCHAR2(200),
+    apellido_paterno       VARCHAR2(200),
+    apellido_materno       VARCHAR2(200),
+    apellido_casada        VARCHAR2(200),
+    fe_nacimiento           DATE,
 
-  v_co_continente_naci VARCHAR2(50);
-  v_co_pais_naci       VARCHAR2(50);
-  v_co_depart_naci     VARCHAR2(50);
-  v_co_prov_naci       VARCHAR2(50);
-  v_co_dist_naci      VARCHAR2(50);
-  v_co_centro_pob_naci VARCHAR2(50);
+    co_continente_naci      VARCHAR2(50),
+    co_pais_naci            VARCHAR2(50),
+    co_departamento_naci   VARCHAR2(50),
+    co_provincia_naci      VARCHAR2(50),
+    co_distrito_naci       VARCHAR2(50),
+    co_centro_pob_naci    VARCHAR2(50),
 
-  v_lugar_nacimiento  VARCHAR2(400);
-  v_ubigeo_nacimiento VARCHAR2(50);
-  v_sexo               VARCHAR2(50);
-  v_estado_civil      VARCHAR2(100);
+    lugar_nacimiento       VARCHAR2(400),
+    ubigeo_nacimiento      VARCHAR2(50),
+    sexo                    VARCHAR2(50),
+    estado_civil           VARCHAR2(100),
 
-  v_foto               BLOB;
+    foto                    BLOB,
 
-  v_ubigeo_domicilio  VARCHAR2(50);
-  v_lugar_domicilio   VARCHAR2(400);
-  v_domicilio         VARCHAR2(4000);
+    ubigeo_domicilio       VARCHAR2(50),
+    lugar_domicilio        VARCHAR2(400),
+    domicilio              VARCHAR2(4000),
 
-  v_grupo_factor      VARCHAR2(100);
-  v_observaciones     VARCHAR2(4000);
-  v_restriccion       VARCHAR2(200);
+    grupo_factor_sanguineo VARCHAR2(100),
+    observaciones          VARCHAR2(4000),
+    restriccion            VARCHAR2(200),
 
-  v_fecha_emision     DATE;
-  v_fecha_caducidad_dnie VARCHAR2(50);
+    fecha_emision_dnie     DATE,
+    fecha_caducidad_dnie   VARCHAR2(50),
 
-  v_tipo_doc_progenitor1 VARCHAR2(50);
-  v_num_doc_progenitor1  VARCHAR2(50);
-  v_tipo_doc_progenitor2 VARCHAR2(50);
-  v_num_doc_progenitor2  VARCHAR2(50);
+    tipo_doc_progenitor1  VARCHAR2(50),
+    num_doc_progenitor1   VARCHAR2(50),
+    tipo_doc_progenitor2  VARCHAR2(50),
+    num_doc_progenitor2   VARCHAR2(50),
 
-  v_email              VARCHAR2(200);
-  v_nacionalidad      VARCHAR2(10);
-BEGIN
-  SP_GET_DATOS_COMPLETO_DNI(
-    p_nu_dni    => '12345678',  -- <- DNI a consultar
-    p_recordset => l_rc
+    email                   VARCHAR2(200),
+    nacionalidad           VARCHAR2(10)
   );
 
-  BEGIN
-    FETCH l_rc INTO
-      v_cui_dni,
-      v_prenombres,
-      v_apellido_paterno,
-      v_apellido_materno,
-      v_apellido_casada,
-      v_fecha_nacimiento,
-      v_co_continente_naci,
-      v_co_pais_naci,
-      v_co_depart_naci,
-      v_co_prov_naci,
-      v_co_dist_naci,
-      v_co_centro_pob_naci,
-      v_lugar_nacimiento,
-      v_ubigeo_nacimiento,
-      v_sexo,
-      v_estado_civil,
-      v_foto,
-      v_ubigeo_domicilio,
-      v_lugar_domicilio,
-      v_domicilio,
-      v_grupo_factor,
-      v_observaciones,
-      v_restriccion,
-      v_fecha_emision,
-      v_fecha_caducidad_dnie,
-      v_tipo_doc_progenitor1,
-      v_num_doc_progenitor1,
-      v_tipo_doc_progenitor2,
-      v_num_doc_progenitor2,
-      v_email,
-      v_nacionalidad;
+  r_reg t_dni;
+BEGIN
+  SP_GET_DATOS_COMPLETO_DNI(
+    p_nu_dni    => '12345678',  -- <- cambia aquí
+    p_recordset => v_cursor
+  );
 
-    DBMS_OUTPUT.PUT_LINE('DNI: ' || v_cui_dni);
-    DBMS_OUTPUT.PUT_LINE('Nombres: ' || v_prenombres);
-    DBMS_OUTPUT.PUT_LINE('Apellidos: ' || v_apellido_paterno || ' ' || v_apellido_materno);
-    DBMS_OUTPUT.PUT_LINE('Email: ' || v_email);
-    DBMS_OUTPUT.PUT_LINE('Sexo: ' || v_sexo);
-    DBMS_OUTPUT.PUT_LINE('Foto bytes: ' ||
-      CASE WHEN v_foto IS NULL THEN 'NULL' ELSE DBMS_LOB.GETLENGTH(v_foto) END);
+  LOOP
+    FETCH v_cursor INTO r_reg;
+    EXIT WHEN v_cursor%NOTFOUND;
 
-  EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-      DBMS_OUTPUT.PUT_LINE('No existe data para ese DNI.');
-  END;
+    DBMS_OUTPUT.PUT_LINE('DNI: ' || r_reg.cui_dni);
+    DBMS_OUTPUT.PUT_LINE('Nombres: ' || r_reg.prenombres);
+    DBMS_OUTPUT.PUT_LINE('Apellidos: ' || r_reg.apellido_paterno || ' ' || r_reg.apellido_materno || ' ' || r_reg.apellido_casada);
+    DBMS_OUTPUT.PUT_LINE('Fecha Nac: ' || TO_CHAR(r_reg.fe_nacimiento, 'DD/MM/YYYY'));
+    DBMS_OUTPUT.PUT_LINE('Sexo: ' || r_reg.sexo);
+    DBMS_OUTPUT.PUT_LINE('Estado Civil: ' || r_reg.estado_civil);
 
-  CLOSE l_rc;
+    DBMS_OUTPUT.PUT_LINE('Domicilio: ' || r_reg.domicilio);
+    DBMS_OUTPUT.PUT_LINE('Restriccion: ' || r_reg.restriccion);
+    DBMS_OUTPUT.PUT_LINE('Fecha Caducidad: ' || r_reg.fecha_caducidad_dnie);
+
+    DBMS_OUTPUT.PUT_LINE('Email: ' || r_reg.email);
+    DBMS_OUTPUT.PUT_LINE('Nacionalidad: ' || r_reg.nacionalidad);
+
+    DBMS_OUTPUT.PUT_LINE(
+      'Foto bytes: ' ||
+      CASE WHEN r_reg.foto IS NULL THEN 'NULL' ELSE DBMS_LOB.GETLENGTH(r_reg.foto) END
+    );
+
+    DBMS_OUTPUT.PUT_LINE('-----------------------------------');
+  END LOOP;
+
+  CLOSE v_cursor;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    IF v_cursor%ISOPEN THEN
+      CLOSE v_cursor;
+    END IF;
+    DBMS_OUTPUT.PUT_LINE('Error detectado: ' || SQLERRM);
 END;
 /
