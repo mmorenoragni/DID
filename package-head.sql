41ef72f07ce4c0c7da621bc50aa4ed938367658b
mmorenoragni <mmorenoragni@gmail.com>
2026-04-25 17:46:26 -0500

    Update package-head.sql

--- package-head.sql
+++ package-head.sql
@@ -14,6 +14,19 @@ CREATE OR REPLACE PACKAGE DDPK_DNI_DNID AS
   -- *************************************************************************************************
   --
   ----------------------------------------------------------------------------------------------------
+  -- Nombre de la Función     : DDFU_GET_DESC_DOMINIO
+  -- Autor                    : Mariano Moreno
+  -- Versión                  : 1.0
+  -- Creado                   : {a definir}
+  -- Modificado               : {a definir}
+  -- Propósito                : OBTENER DESCRIPCIÓN DEL DOMINIO
+  -- Requerimientos           : Conexión con la base de datos DNI - {a definifr}.
+  -- Parámetros               : p_vNO_DOMINIO            (IN) : NOMBRE DEL DOMINIO
+  --                            p_vCO_DOMINIO            (IN) : CÓDIGO DEL DOMINIO
+  ----------------------------------------------------------------------------------------------------
+  FUNCTION DDFU_GET_DESC_DOMINIO(p_vNO_DOMINIO VARCHAR2,
+                                 p_vCO_DOMINIO VARCHAR2) RETURN VARCHAR2;
+  ----------------------------------------------------------------------------------------------------
   -- Nombre del Procedimiento : DDSP_GET_DATOS_COMPLETO_DNI
   -- Autor                    : Mariano Moreno
   -- Versión                  : 1.0
@@ -42,7 +55,9 @@ CREATE OR REPLACE PACKAGE DDPK_DNI_DNID AS
   --                            DOMICILIO                (OUT) : DOMICILIO NUMERICO Y CALLE
   --                            GRUPO_FACTOR_SANGUINEO   (OUT) : FACTOR SANGUINEO DE LA PERSONA
   --                            OBSERVACIONES            (OUT) : LISTA DE OBSERVACIONES ADJUNTAS
+  --                            CO_RESTRI                (OUT) : CODIGO RESTRICCION DEL USUARIO
   --                            RESTRICCION              (OUT) : RESTRICCIONES DEL USUARIO
+  --                            CO_GRUPO_RESTRI          (OUT) : CODIGO GRUPO RESTRICCION DEL USUARIO
   --                            FECHA_EMISION_DNIE       (OUT) : FECHA EN QUE SE EMITIO EL DNID
   --                            FECHA_CADUCIDAD_DNIE     (OUT) : FECHA DE EXPIRACION DEL DNID
   --                            TIPO_DOC_PROGENITOR1     (OUT) : TIPO DE DOCUMENTO DEL PADRE
@@ -54,6 +69,7 @@ CREATE OR REPLACE PACKAGE DDPK_DNI_DNID AS
   ----------------------------------------------------------------------------------------------------
   PROCEDURE DDSP_GET_DATOS_COMPLETO_DNI(p_nu_dni    IN VARCHAR2,
                                         p_recordset OUT SYS_REFCURSOR);
+  ----------------------------------------------------------------------------------------------------
   -- Nombre del Procedimiento : DDSP_INSERT_DNID_LOG
   -- Autor                    : Mariano Moreno
   -- Versión                  : 1.0
@@ -61,12 +77,14 @@ CREATE OR REPLACE PACKAGE DDPK_DNI_DNID AS
   -- Modificado               : {a definir}
   -- Propósito                : OBTENER INFORMACION NECESARIA PARA LA CREACION DE CREDENCIALES VERIFICABLES DNIDd
   -- Requerimientos           : Conexión con la base de datos DNI - {a definifr}.
-  -- Parámetros               : p_nu_dni                 (IN)  : NUMERO DE DNIe
+  -- Parámetros               : p_id                          (IN)  : Identificador unico para el registro
+  -- Parámetros               : p_mensaje_log                 (IN)  : Mensaje con el contenido del log que se quiere guardar
+  -- Parámetros               : p_dni_solicitante             (IN)  : Número de documento del solicitante de la credencial
+  -- Parámetros               : p_dni_solicitado              (IN)  : Número de documento solicitado para imprimir la credencial
   ----------------------------------------------------------------------------------------------------
-  PROCEDURE DDSP_INSERT_DNID_LOG(
-  p_id              IN dnid_logs.id%TYPE,
-  p_mensaje_log     IN dnid_logs.mensaje_log%TYPE,
-  p_dni_solicitante IN dnid_logs.dni_solicitante%TYPE DEFAULT NULL,
-  p_dni_solicitado  IN dnid_logs.dni_solicitado%TYPE  DEFAULT NULL
-);
+  PROCEDURE DDSP_INSERT_DNID_LOG(p_id              IN dnid_logs.id%TYPE,
+                                 p_mensaje_log     IN dnid_logs.mensaje_log%TYPE,
+                                 p_dni_solicitante IN dnid_logs.dni_solicitante%TYPE DEFAULT NULL,
+                                 p_dni_solicitado  IN dnid_logs.dni_solicitado%TYPE DEFAULT NULL);
+  ----------------------------------------------------------------------------------------------------
 END;
